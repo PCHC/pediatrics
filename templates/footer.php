b69e4c535354832bc22f3d40f491349140ee9dc7
Chris Violette <cvioletterun@gmail.com>
2014-11-17 11:32:48 -0500

    Added copyright information

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,12 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
+  	<div class="copyright">
+  		&copy; <?php echo strftime('%Y'); ?> <?php bloginfo('name'); ?>
+  	</div>
+  	<div class="footer-locations">
+  		<em>Penobscot Pediatrics</em> | 1068 Union Street, Bangor, ME 04401 | <em>207-947-0147</em><br>
+  		<em>Helen Hunt Health Center</em> | 242 Brunswick St, Old Town, ME 04468 | <em>207-827-6128</em>
+  	</div>
     <?php dynamic_sidebar('sidebar-footer'); ?>
   </div>
 </footer>
