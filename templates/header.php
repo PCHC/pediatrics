1df63d4e4f1f5ea21d2e05f5d43357b465d22c48
Chris Violette <cvioletterun@gmail.com>
2014-11-17 15:48:14 -0500

    A whole bunch of mobile styling

--- templates/header.php
+++ templates/header.php
@@ -2,16 +2,14 @@
   <div class="navbar navbar-main">
     <div class="container">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse-primary">
           <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
+          <i class="fa fa-bars"></i>
         </button>
         <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
       </div>
 
-      <nav class="collapse navbar-collapse" role="navigation">
+      <nav class="collapse navbar-collapse navbar-collapse-primary" role="navigation">
         <?php
           if (has_nav_menu('primary_navigation')) :
             wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav'));
@@ -22,7 +20,11 @@
   </div>
   <div class="navbar navbar-secondary">
     <div class="container">
-      <nav class="collapse navbar-collapse" role="navigation">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-secondary">
+        <span class="sr-only">Toggle navigation</span>
+        <i class="fa fa-bars"></i>
+      </button>
+      <nav class="collapse navbar-collapse navbar-collapse-secondary" role="navigation">
         <?php
           if (has_nav_menu('secondary_navigation')) :
             wp_nav_menu(array('theme_location' => 'secondary_navigation', 'menu_class' => 'nav navbar-nav'));
